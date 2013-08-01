51d67eda7649b36886227348573920d2a0acb2b6
Łukasz <lukasz.zdanikowski@gmail.com>
2013-08-01 08:53:57 +0200

    Update to unicode_name.rb to read data in windows cp1251 encoding
    quick fix to read psd files with text in cp1251 encoding

--- lib/psd/layer_info/unicode_name.rb
+++ lib/psd/layer_info/unicode_name.rb
@@ -7,7 +7,7 @@ class PSD
     def parse
       pos = @file.tell
       len = @file.read_int * 2
-      @data = @file.read(len).unpack("A#{len}")[0].encode('UTF-8').delete("\000")
+      @data = @file.read(len).unpack("A#{len}")[0].force_encoding('cp1251').encode('UTF-8').delete("\000")
 
       # The name seems to be padded with null bytes. This is the easiest solution.
       @file.seek pos + @length
